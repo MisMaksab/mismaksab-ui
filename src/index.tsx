65c520fd571086c99e52020ec2c68e1adf8464a7
grigorevpv <grigorevpv@gmail.com>
2023-08-06 17:49:25 +0300

    Export footer

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ export { Button } from "./common/Button/Button"
 // Desktop
 export * as variables from "./scss/variables.scss";
 export { DesktopHeader } from "./desktop/DesktopHeader/DesktopHeader";
+export { DesktopFooter } from "./desktop/DesktopFooter/DesktopFooter";
+export { DesktopFooterLinks } from "./desktop/DesktopFooterLinks/DesktopFooterLinks";
+export { DesktopFooterInfo } from "./desktop/DesktopFooterInfo/DesktopFooterInfo";
 export { Info } from "./desktop/Info/Info";
 export { LanguageDropdown } from "./common/LanguageDropdown/LanguageDropdown";
 export { DesktopHeaderLogo } from "./desktop/DesktopHeaderLogo/DesktopHeaderLogo";
