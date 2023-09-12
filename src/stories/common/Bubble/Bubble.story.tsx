d79bac5ef47683cc2a5e552d2f8b128bd5a8d83b
imgay <temadoron9@gmail.com>
2023-09-12 20:39:51 +0300

    callback fixed

--- src/stories/common/Bubble/Bubble.story.tsx
+++ src/stories/common/Bubble/Bubble.story.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Bubble } from "../../../common/Bubble/Bubble";
 
 export const BubbleStory = () => (
-  <Bubble mode="orange" text="товары" layout="desktop" link="/"/>
+  <Bubble mode="orange" text="товары" layout="desktop" dropdown={<span>text</span>}/>
 );
 
 BubbleStory.storyName = "Header";
