c277da0ba691063fc989dd3b7b5a5bd22a1d784f
imgay <temadoron9@gmail.com>
2023-10-18 01:58:14 +0300

    err

--- tmp/src/stories/common/ExpandedGoodCard/ExpandedGoodCard.story.tsx
+++ tmp/src/stories/common/ExpandedGoodCard/ExpandedGoodCard.story.tsx
@@ -77,7 +77,7 @@ ExpadnedGoodCardStory.args = {
     goToRetailerLink: '/',
     discountConditionsText: 'Скидка при предъявлении карты магазина',
     retailerCardImage: 'rimi',
-    layout: 'mobile'
+    layout: 'desktop'
 }
 
 ExpadnedGoodCardStory.storyName = "ExpandedGoodCard";
