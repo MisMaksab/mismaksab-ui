0cf38401aab3b47efef547967681ebf2305599f8
grigorevpv <grigorevpv@gmail.com>
2023-11-04 13:50:55 +0200

    Remove unused code

--- tmp/src/stories/mobile/RetailersSlider/RetailersSlider.story.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { MobileRetailersSlider } from "../../../mobile/MobileRetailersSlider/MobileRetailersSlider";
-
-export const RetailersSliderStory = () => (
-  <MobileRetailersSlider slidesData={[1,2,3,4,5,6,7,8,9]}/>
-);
-
-RetailersSliderStory.storyName = "mobile retailers slider";
